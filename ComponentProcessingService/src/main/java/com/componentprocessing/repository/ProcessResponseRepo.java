6a8ffe898afcd0ee5816cf7c1d735c3fc60cf123
Satyapriyadas48 <98417530+Satyapriyadas48@users.noreply.github.com>
2022-05-27 10:49:13 +0530

    Delete ComponentProcessingService directory

--- ComponentProcessingService/src/main/java/com/componentprocessing/repository/ProcessResponseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.componentprocessing.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.componentprocessing.entity.ProcessResponse;
-
-public interface ProcessResponseRepo extends JpaRepository<ProcessResponse, Integer>{
-
-}
